a4f20f6538c7b26e7b3babb63a3ef0fde7f3dfb3
Alejandro Romero <alejo8591@gmail.com>
2012-04-05 01:06:30 -0500

    Changes in model app car

--- toromobile/car/models.py
+++ toromobile/car/models.py
@@ -31,7 +31,7 @@ class CarLine(models.Model):
 #Color de los carros referente MINMCOLP    
 class CarColor(models.Model):
     toro_cc_code = models.IntegerField(max_length=4, verbose_name='Código', help_text="Código correspondiente del Color")
-    toro_cc_description = models.CharField(max_length=4, verbose_name='Descripción', help_text="Nombre completo del Color")
+    toro_cc_description = models.CharField(max_length=20, verbose_name='Descripción', help_text="Nombre completo del Color")
     def __unicode__(self):
        return u'%s %s' %(self.toro_cc_code, self.toro_cc_description)
         
@@ -41,12 +41,12 @@ class CarColor(models.Model):
 #Tipo de Carroceria de los Carros referente a MINMTICARROC
 class CarBodyWork(models.Model):
     toro_cbw_code = models.IntegerField(max_length=3, verbose_name='Código', help_text="Código correspondiente a la carroceria")
-    toro_cbw_description = models.CharField(max_length=4, verbose_name='Descripción', help_text="Nombre completo del tipo de carroceria")
+    toro_cbw_description = models.CharField(max_length=20, verbose_name='Descripción', help_text="Nombre completo del tipo de carroceria")
     def __unicode__(self):
        return u'%s %s' %(self.toro_cbw_code, self.toro_cbw_description)
         
     class Meta:
-        ordering = ['toro_cc_code']
+        ordering = ['toro_cbw_code']
         
 #Datos del dueño del Vehiculo
 class CarOwner(models.Model):
@@ -77,24 +77,24 @@ class CarMandated(models.Model):
 #Clase general para crear el carro
 class Car(models.Model):
    toro_car_plate = models.CharField(max_length=6, verbose_name='Placa Vehiculo', help_text="Tres letras y tres número ABC123 Sin espacios")
-   toro_car_mark = models.ForeignKey(CarMark)
-   toro_car_line = models.ForeignKey(CarLine)
+   toro_car_mark = models.ForeignKey(CarMark, verbose_name='Marca Vehiculo')
+   toro_car_line = models.ForeignKey(CarLine, verbose_name='Linea Vehiculo')
    toro_car_model_year =  models.DateField(verbose_name='Modelo', help_text="Ingrese el Modelo del año de producción del Carro Ejemplo: 2001")
    toro_car_model_power = models.DateField(verbose_name='Modelo Repotenciado', help_text="año a que haya sido repotenciado o transformado un vehículo", blank=True)
    toro_car_num_serial = models.CharField(max_length=25, verbose_name='Número Serie', help_text="Número de serie del Vehiculo")
    toro_car_color = models.ForeignKey(CarColor, verbose_name='Color Vehiculo')
-   toro_car_bodyWork = models.ForeignKey(CarBodyWork)
+   toro_car_bodyWork = models.ForeignKey(CarBodyWork, verbose_name='Tipo de Carroceria')
    toro_car_kilo = models.FloatField(max_length=5, verbose_name='Peso (Kg.)', help_text="Peso del vehiculo vacio en Kilogramos(Kg.)")
    toro_car_register = models.IntegerField(max_length=8, verbose_name='Registro Nacional', help_text="Número de registro Nacional del Vehiculo")
    toro_car_soat = models.CharField(max_length=20, verbose_name='S.O.A.T', help_text="Número de poliza del S.O.A.T")
    toro_car_id_insurance = models.IntegerField(max_length=11, verbose_name='NIT Aseguradora', help_text="Número de identificación de la aseguradora")
    toro_car_soat_date = models.DateField(verbose_name='Fecha Vencimiento S.O.A.T', help_text="Fecha de vencimiento de la Poliza ")
-   toro_car_owner = models.ForeignKey(CarOwner)
-   toro_car_mandated = models.ForeignKey(CarMandated)
-   date_pub_event = models.DateField(default=datetime.now, auto_now = False, editable=False, verbose_name='Fecha de creación vehiculo')
+   toro_car_owner = models.ForeignKey(CarOwner, verbose_name='Dueño Vehiculo')
+   toro_car_mandated = models.ForeignKey(CarMandated, verbose_name='Tenedor Vehiculo')
+   toro_date_car_created = models.DateField(default=datetime.now, auto_now = False, editable=False, verbose_name='Fecha de creación vehiculo')
    #Optimización para ver en el admin site de Django
    def __unicode__(self):
-       return u'%s %s' %(self.toro_car_plate, self.toro_car_bodyWork)
+       return u'%s %s' %(self.toro_car_plate, self.toro_date_car_created)
         
    class Meta:
         ordering = ['toro_car_plate']
