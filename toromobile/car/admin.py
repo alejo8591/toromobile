9d38dab5077383a6d092c17657d9e0d89663adb3
Alejandro Romero <alejo8591@gmail.com>
2012-04-07 14:49:06 -0500

    Update admin for app car

--- toromobile/car/admin.py
+++ toromobile/car/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.db import models
 from django.forms.extras.widgets import SelectDateWidget
-from car.models import Car, CarBodyWork, CarColor, CarLine, CarMandated, CarMark, CarOwner
+from car.models import Car, CarBodyWork, CarColor, CarLine, CarMandated, CarMark, CarOwner, CarTrailer
 from car.SelectTimeWidget import SelectTimeWidget
     
 class CarBodyWorkAdmin(admin.ModelAdmin):
@@ -15,13 +15,16 @@ class CarLineAdmin(admin.ModelAdmin):
     list_display = ('toro_cl_code', 'toro_cl_description')
     
 class CarMandatedAdmin(admin.ModelAdmin):
-    list_display = ('toro_cm_type_id', 'toro_cm_owner_first_name', 'toro_cm_owner_last_name')
+    list_display = ('toro_cm_type_id', 'toro_cm_first_name', 'toro_cm_last_name')
     
 class CarMarkAdmin(admin.ModelAdmin):
     list_display = ('toro_cm_code', 'toro_cm_code')
 
 class CarOwnerAdmin(admin.ModelAdmin):
-    list_display = ('toro_co_type_id', 'toro_co_owner_first_name', 'toro_co_owner_last_name')
+    list_display = ('toro_co_type_id', 'toro_co_first_name', 'toro_co_last_name')
+    
+class CarTrailerAdmin(admin.ModelAdmin):
+    list_display = ('toro_ct_id', 'toro_ct_description')
 
 class CarAdmin(admin.ModelAdmin):
     #Cambiando modo en el que se ve el TimeField con respecto al admin de django
@@ -31,7 +34,7 @@ class CarAdmin(admin.ModelAdmin):
         #Cambiando la manera de ver el DateTime con respecto al admin    
         models.DateField: {'widget': SelectDateWidget}
     }
-    list_display = ('toro_car_plate', 'toro_date_car_created')    
+    list_display = ('toro_car_plate', 'toro_car_date_created')    
 #Adicionando a django admin 
 admin.site.register(CarBodyWork, CarBodyWorkAdmin)
 admin.site.register(CarColor, CarColorAdmin)
@@ -40,3 +43,4 @@ admin.site.register(CarMandated, CarMandatedAdmin)
 admin.site.register(CarMark, CarMarkAdmin)
 admin.site.register(CarOwner, CarOwnerAdmin)
 admin.site.register(Car, CarAdmin)
+admin.site.register(CarTrailer, CarTrailerAdmin)
