22647ed8f46f432ad4d1bc30fe7a70f446b02726
Alejandro Romero <alejo8591@gmail.com>
2012-04-10 20:57:11 -0500

    correcting bugs

--- toromobile/driver/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.db import models
-from driver.models import CarDriver
-
-class CarDriverAdmin(admin.ModelAdmin):
-    list_display = ('toro_cd_id', 'toro_cd_first_name', 'toro_cd_name')
-    
-admin.site.register(CarDriver, CarDriverAdmin)
