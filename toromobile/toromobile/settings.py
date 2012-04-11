44572551a7b6042e60fd0bbe4aa648e6a8f3afae
Alejandro Romero <alejo8591@gmail.com>
2012-04-11 00:11:43 -0500

    Update install the grappelli admin interface and other configs

--- toromobile/toromobile/settings.py
+++ toromobile/toromobile/settings.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 # Django settings for toromobile project.
-import os
-import django
 DEBUG = True
-#Full filesystem path to the project.
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Name of the directory for the project.
-#PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
+__version__ = "0.0.1"
+__status__ = "alpha"
+
+# Get the current directory
+import os
+ROOT_CONF = os.path.dirname(os.path.realpath(__file__))
 
 TEMPLATE_DEBUG = DEBUG
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
-DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -22,65 +20,34 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'toromobile',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'toromobile',                     
+        'USER': 'root',                   
+        'PASSWORD': '',                 
+        'HOST': '',                      
+        'PORT': '',                      
     }
 }
+#django debug toolbar
 INTERNAL_IPS = ('127.0.0.1',)
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/Bogota'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'es-es'
+LANGUAGES = (
+    ('es_ES', 'Espanol'),
+    ('en_GB', 'English'),
+)
 
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(SITE_ROOT, 'assets')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
+MEDIA_ROOT = ROOT_CONF + '/uploads/'
+MEDIA_URL = '/uploads/'
+STATIC_ROOT = ROOT_CONF + '/static/'
+STATIC_URL = '/static'
+ADMIN_MEDIA_PREFIX = STATIC_URL + '/grappelli/'
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    (ROOT_CONF + '/static_files/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,7 +76,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'toromobile.urls'
@@ -121,9 +88,17 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'templates'),
+    (ROOT_CONF + '/templates'),
     ("/.virtualenvs/ukelele/lib/python2.7/site-packages/django-debug-toolbar/debug_toolbar"),
 )
+
+GRAPPELLI_ADMIN_TITLE = "<li class='user-options-container collapse closed'> \
+<a href='javascript://' class='user-options-handler collapse-handler'> \
+toromobile %s</a><ul class='user-options'><li><a href='/' \
+style='padding:10px;'>Back to site</a></li></ul></li>" % (__version__) %(_)
+#GRAPPELLI_ADMIN_URL = '/admin'
+#GRAPPELLI_INDEX_DASHBOARD = 'toromobile.dashboard.CustomIndexDashboard'
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -132,6 +107,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'grappelli.dashboard',
+    'grappelli',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -147,11 +124,6 @@ INSTALLED_APPS = (
     'manifest',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
