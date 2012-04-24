e8c7053cccde9ee953c881ff3eba549f1b3bf149
Alejandro Romero <alejo8591@gmail.com>
2012-04-24 14:37:05 -0500

    update test for city app

--- toromobile/city/tests.py
+++ toromobile/city/tests.py
@@ -4,22 +4,37 @@ from django.test import TestCase
 from django.db import IntegrityError
 from city.models import CodeDANE
 from datetime import datetime
+from django.conf import Settings
 """
-    toro_cd_id_city = models.IntegerField(max_length=10, verbose_name='Código Ciudad', help_text="Código de identificación de la ciudad según DANE", unique=True)
-    toro_cd_name_city = models.CharField(max_length=20, verbose_name='Nombre Ciudad', help_text="Nombre de la ciudad, municipio, cacerio según DANE")
-    toro_cd_dpto = models.CharField(max_length=20, verbose_name='Nombre Departamento', help_text="Nombre de identificación de la ciudad según DANE")
+    Data struct in models city
+    toro_cd_id_city :  int
+    toro_cd_name_city : string
+    toro_cd_dpto : string
 """
-class CodeDANETestCase(TestCase):
-    """" Test sobre los Metodos de guardado """
-    id_city = 11001
-    name_city = "Bogotá"
-    dpto = "Bogota D.C."
-    
-    def setUp(self):
-        self.bogota = CodeDANE.objects.create(toro_cd_id_city=self.id_city,
-                                              toro_cd_name_city= self.name_city,
-                                              toro_cd_dpto= self.dpto)
 
-        
-    def test_model(self):
-        self.assertAlmostEqual(self.bogota.toro_cd_id_city, self.id_city)
+MODELS= [CodeDANE]
+
+#Inegrity in unicode
+class  CodeDANEUnicodeTest(TestCase):
+    def testUnicode(self):
+        id_city = 13212
+        name_city = u'Córdoba'
+        dpto_city = u'Bolívar'
+        s = CodeDANE.objects.create(toro_cd_id_city=id_city, toro_cd_name_city=name_city, toro_cd_dpto=dpto_city)
+        self.assertEqual(unicode(s), u'13212 Córdoba Bolívar')
+"""             
+    def tearDown(self):
+        'Depopulate created model instances from test database.'
+        for model in MODELS:
+            for obj in model.objects.all():
+                obj.delete()
+                
+        self.assertEqual(CodeDANE.objects.all().count(), 0)
+"""
+#fixtures
+class CodeDANEFixtures(TestCase):
+    fixtures = ['../city/fixtures/test_codedane.json']
+    
+    def testCodeDADE(self):
+        q = CodeDANE.objects.all().filter(toro_cd_id_city=63212)
+        self.assertEqual(q.count(), 1)
