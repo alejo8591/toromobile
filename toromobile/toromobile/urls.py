87e00350ab368a285a707aff1991b89745332e43
Alejandro Romero <alejo8591@gmail.com>
2012-05-03 00:12:41 -0500

    update the urls of toromobile

--- toromobile/toromobile/urls.py
+++ toromobile/toromobile/urls.py
@@ -1,4 +1,10 @@
 from django.conf.urls import patterns, include, url
+from toromobile.views import current_datetime
+from django.conf import settings
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import ugettext_lazy as _
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,15 +12,26 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'toromobile.views.home', name='home'),
-    #url(r'^$', include(admin.site.urls)),
-    
+    url(r'^$', current_datetime, name='home'),
     # url(r'^toromobile/', include('toromobile.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^grappelli/', include('grappelli.urls')),
+    (r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
+        # Static content #### FOR DEVELOPMENT!! ####
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': 'static'}),
 )
+
+urlpatterns += patterns('',
+    # Static content #### FOR DEVELOPMENT!! ####
+    url(r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
+)
+
+if settings.DEBUG:
+    # Serve static files
+    urlpatterns += staticfiles_urlpatterns()
