b63e2a780a6da8b35a23187f6091983deee0ba54
Alejandro Romero <alejo8591@gmail.com>
2012-05-03 00:20:41 -0500

    update the settings files

--- toromobile/toromobile/settings.py
+++ toromobile/toromobile/settings.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Django settings for toromobile project.
+
 DEBUG = True
 
 __version__ = "0.0.1"
@@ -12,35 +12,60 @@ ROOT_CONF = os.path.dirname(os.path.realpath(__file__))
 TEMPLATE_DEBUG = DEBUG
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 
+
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Alejandro Romero', 'alejo8591@gmail.com'),
 )
 
 MANAGERS = ADMINS
+"""
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', 
+            'NAME': 'heroku_210e8083675e5dd',                     
+            'USER': 'f4c58c2ba25df7',                     
+            'PASSWORD': 'ea7dc267',                 
+            'HOST': '',                     
+            'PORT': '',               
+            }
+}
+"""
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'toromobile',                     
-        'USER': 'root',                   
-        'PASSWORD': '',                 
-        'HOST': '',                      
-        'PORT': '',                      
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', 
+            'NAME': 'toromobileh',                     
+            'USER': 'root',                     
+            'PASSWORD': '',                 
+            'HOST': '',                     
+            'PORT': '',               
+            }
 }
+
 #django debug toolbar
 INTERNAL_IPS = ('127.0.0.1',)
 TIME_ZONE = 'America/Bogota'
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'es-ES'
 LANGUAGES = (
     ('es_ES', 'Espanol'),
     ('en_GB', 'English'),
 )
 
 SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
 USE_I18N = True
 USE_L10N = True
-MEDIA_ROOT = ROOT_CONF + '/uploads/'
+MEDIA_ROOT = ROOT_CONF
 MEDIA_URL = '/uploads/'
 STATIC_ROOT = ROOT_CONF + '/static/'
 STATIC_URL = '/static'
@@ -49,7 +74,6 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + '/grappelli/'
 STATICFILES_DIRS = (
     (ROOT_CONF + '/static_files/'),
 )
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -59,7 +83,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'l*idmu_ddhl^8454a2l&amp;9nnt(fel7u=6q9htegbcdk(d5_3235'
+SECRET_KEY = '3b0iu=j40o^iwzyt7_@a5p=6%@@=gk8g&amp;+3jgw#1re7ls**(3h'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -76,7 +100,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'toromobile.urls'
@@ -85,20 +108,17 @@ ROOT_URLCONF = 'toromobile.urls'
 WSGI_APPLICATION = 'toromobile.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     (ROOT_CONF + '/templates'),
-    ("/.virtualenvs/ukelele/lib/python2.7/site-packages/django-debug-toolbar/debug_toolbar"),
 )
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 
 GRAPPELLI_ADMIN_TITLE = "<li class='user-options-container collapse closed'> \
 <a href='javascript://' class='user-options-handler collapse-handler'> \
 toromobile %s</a><ul class='user-options'><li><a href='/' \
-style='padding:10px;'>Back to site</a></li></ul></li>" % (__version__) %(_)
+style='padding:10px;'>Back to site</a></li></ul></li>" % (__version__)
 #GRAPPELLI_ADMIN_URL = '/admin'
 #GRAPPELLI_INDEX_DASHBOARD = 'toromobile.dashboard.CustomIndexDashboard'
-
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -107,21 +127,25 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'grappelli.dashboard',
+    #'devserver',
     'grappelli',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django_extensions',
-    'debug_toolbar',
-    'devserver',
+    #'django_extensions',
+    #'debug_toolbar',
     'car',
     'city',
-    'driver',
     'company',
-    'package',
-    'manifest',
+    'despatch',
+    'driver',
+    'packet',
+    'routes'
+)
+
+FIXTURE_DIRS = (
+   '/Users/alejo8591/.virtualenvs/toromobile/vivid-ocean-1546/city/fixtures/',
 )
 
 LOGGING = {
